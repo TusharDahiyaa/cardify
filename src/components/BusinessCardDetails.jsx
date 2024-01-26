88de6f19e1b531f18cb40f98ebb9c8a4a2ab3278
Tushar Dahiya <tushardahiya202@gmail.com>
2024-01-27 02:08:32 +0530

    Updated phone number input field with country selection

--- src/components/BusinessCardDetails.jsx
+++ src/components/BusinessCardDetails.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 import { Button } from "flowbite-react";
 import { Report } from "notiflix/build/notiflix-report-aio";
+import "react-phone-number-input/style.css";
+import PhoneInput, { isPossiblePhoneNumber } from "react-phone-number-input";
 
 export default function BusinessCardDetails() {
+  const [phoneValue, setPhoneValue] = useState();
+
   const saveBusinessCard = async (data) => {
     try {
       const token = window.localStorage.getItem("token");
@@ -17,8 +21,6 @@ export default function BusinessCardDetails() {
         body: JSON.stringify(data),
       });
 
-      const responseData = await response.json();
-
       if (response.ok) {
         // console.log("Business card saved successfully");
         Report.success(
@@ -26,7 +28,7 @@ export default function BusinessCardDetails() {
           "Successfully created the business card!",
           "Okay",
           () => {
-            window.location.href = "/businessCard";
+            // window.location.href = "/businessCard";
           }
         );
       } else {
@@ -47,10 +49,8 @@ export default function BusinessCardDetails() {
       data[key] = value;
     });
 
-    // Now 'data' contains the form field values, and you can use it as needed
-    // console.log(data);
+    data.phone = phoneValue;
 
-    // Add your logic to save the data to the backend (e.g., using an API call)
     saveBusinessCard(data);
   };
 
@@ -115,21 +115,25 @@ export default function BusinessCardDetails() {
             </label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
-            <input
-              type="tel"
-              name="phone"
-              id="phone"
-              pattern="[0-9]{10}"
-              className="block py-2.5 px-0 w-full text-sm text-gray-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              autoComplete="on"
-            />
             <label
               htmlFor="phone"
               className="peer-focus:font-medium absolute text-md text-gray-400 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Phone number(Optional)
             </label>
+            <PhoneInput
+              id="phone"
+              className=" bg-transparent pt-3 "
+              placeholder="Enter phone number"
+              defaultCountry="IN"
+              value={phoneValue}
+              onChange={setPhoneValue}
+              error={
+                phoneValue && isPossiblePhoneNumber(phoneValue)
+                  ? "true"
+                  : "false"
+              }
+            />
           </div>
           <h4 className="mb-4 mt-10 text-gray-500 text-xl underline decoration-sky-500 decoration-double">
             SOCIALS
